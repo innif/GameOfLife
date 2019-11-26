cf7386ac16abfb9a32382cd3438b1cf86078f845
Finn <post@finn-harms.de>
2019-11-26 23:32:07 +0100

    switched to pygame instead of tkinter

--- display.py
+++ display.py
@@ -1,32 +1,32 @@
 import tkinter as tk
+import pygame
+import sys
 
 class Display():
     def __init__(self, fieldSize, windowHeight = 400):
+        pygame.init()
         self.fieldSize = w, h = fieldSize 
         self.windowHeight = windowHeight
         self.blockSize = windowHeight/h
-        self.window = tk.Tk('Conways Game of Life')
-        self.canvas = tk.Canvas(master=self.window, width=self.blockSize*w, height=self.blockSize*h)
-        self.canvas.pack()
+        self.blockSizePixel = round(self.blockSize)
+        self.screen = pygame.display.set_mode((int(self.blockSize*w), int(self.blockSize*h)))
 
     def drawPixel(self, pos):
         x, y = pos
         xPixel, yPixel = int(x*self.blockSize), int(y*self.blockSize)
-        self.canvas.create_rectangle(xPixel, yPixel, xPixel+self.blockSize, yPixel+self.blockSize, fill="blue", outline="blue", tags='pixel')
+        pygame.draw.rect(self.screen, (0,0,255), (xPixel, yPixel, self.blockSizePixel, self.blockSizePixel))
 
     def drawField(self, field):
+        self.screen.fill((200,200,200))
         w, h = self.fieldSize
-        try:
-            self.canvas.delete('all')
-        except:
-            pass
-        
         for y in range(0, h):
             for x in range(0, w):
                 pos = x,y
                 if field.getPixel(pos) == 1:
                     self.drawPixel(pos)
-
+        
     def mainloop(self):
-        self.window.update_idletasks()
-        self.window.update()
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
