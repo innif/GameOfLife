257eeae2b89b2c266687b2e14dc2b03302cb747f
Finn <post@finn-harms.de>
2019-11-27 01:05:21 +0100

    Performance improvements and + placeFigure

--- field.py
+++ field.py
@@ -13,7 +13,7 @@ class Field():
         if seed is not None:
             random.seed(seed)
         w, h = self.size
-        r = np.random.rand(h, w)
+        r = np.random.rand(h, w).astype(np.uint8)
         self.field = np.where(r > 0.5, 1, 0)
 
     def fillField(self, value):
@@ -21,7 +21,14 @@ class Field():
 
     def setField(self, field):
         #TODO Fehlererkennung
-        self.field = np.array(field)
+        self.field = np.array(field).astype(np.uint8)
+
+    def placeFigure(self, figure, offset = (0,0)):
+        #TODO Fehlererkennung
+        h, w = np.array(figure).shape
+        x1, y1 = offset
+        x2, y2 = x1+w, y1+h
+        self.field[y1:y2, x1:x2] = figure
 
     def getField(self):
         return self.field
@@ -37,11 +44,16 @@ class Field():
 
     def update(self):
         w, h = self.size
-        newField = [[self._newState((x,y)) for x in range(w)] for y in range(h)]
-        self.field = np.array(newField)
+        #newField = [[self._newState((x,y)) for x in range(w)] for y in range(h)]
+        newField = np.zeros((h, w), np.uint8)
+        for y in range(h):
+            for x in range(w):
+                newField[y, x] = self._newState((x, y))
+        self.field = newField
 
     def loadFromFile(self, path):
-        pass #TODO implement Method
+        #TODO
+        pass
 
     def print(self):
         print('Field: ')
@@ -54,25 +66,30 @@ class Field():
 
     def _generateField(self, value):
         w, h = self.size
-        field = np.full((h, w), value)
+        field = np.full((h, w), value).astype(np.uint8)
         return field
 
     def _newState(self, pos):
-        state = self.getPixel(pos)
         neighbours = self._countNeighbours(pos)
+
+        if(neighbours < 3):
+            return 0
+            
+        state = self.getPixel(pos)
 
         if(state == 0 and neighbours == 3):
             return 1 # wird geboren
 
-        if(state == 1 and neighbours < 2):
+        if(state == 1 and neighbours < 3):
             return 0 # stirbt an Einsamkeit
 
-        if(state == 1 and neighbours > 3):
+        if(state == 1 and neighbours > 4):
             return 0 # stirbt wegen Überbevölekrung
 
         return state
 
     def _countNeighbours(self, pos, throughWall = False):
+        ''' counts the number of not zero values in the neighbourhood of pos, including pos itself'''
         w, h = self.size
         x, y = pos
 
@@ -84,8 +101,7 @@ class Field():
             x1, x2 = max(x-1, 0), min(x+2, w)
             y1, y2 = max(y-1, 0), min(y+2, h)
 
-        subField = self.field[y1:y2, x1:x2] #FIXME funktioniert nicht
-        neighbours = np.sum(subField)
-        neighbours -= self.getPixel(pos)
+        subField = self.field[y1:y2, x1:x2]
+        neighbours = np.count_nonzero(subField)
         return neighbours
 
