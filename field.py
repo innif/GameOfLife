f78a9aa846543b64e8d70f4ccc4bfb94ee25ffc7
Finn <post@finn-harms.de>
2019-11-27 15:03:56 +0100

    Performance boost

--- field.py
+++ field.py
@@ -5,6 +5,9 @@ class Field():
     def __init__(self, size, initValue = 0):
         self.size = w, h = size
         self.field = self._generateField(initValue)
+        self.numberOfNeighbours = np.zeros(self.size, np.int8)
+        self.newNumberOfNeighbours = np.zeros(self.size, np.int8)
+        self._initializeCountNeighbours()
 
     def getSize(self):
         return self.size
@@ -13,15 +16,18 @@ class Field():
         if seed is not None:
             random.seed(seed)
         w, h = self.size
-        r = np.random.rand(h, w).astype(np.uint8)
-        self.field = np.where(r > 0.5, 1, 0)
+        r = np.random.rand(h, w)
+        self.field = np.where(r > 0.5, 1, 0).astype(np.int8)
+        self._initializeCountNeighbours()
 
     def fillField(self, value):
         self.field = self._generateField(value)
+        self._initializeCountNeighbours()
 
     def setField(self, field):
         #TODO Fehlererkennung
-        self.field = np.array(field).astype(np.uint8)
+        self.field = np.array(field).astype(np.int8)
+        self._initializeCountNeighbours()
 
     def placeFigure(self, figure, offset = (0,0)):
         #TODO Fehlererkennung
@@ -29,13 +35,10 @@ class Field():
         x1, y1 = offset
         x2, y2 = x1+w, y1+h
         self.field[y1:y2, x1:x2] = figure
+        self._initializeCountNeighbours()
 
     def getField(self):
         return self.field
-
-    def setPixel(self, pos, state):
-        x, y = pos
-        self.field[y, x] = state
 
     def getPixel(self, pos):
         x, y = pos
@@ -43,16 +46,19 @@ class Field():
         return self.field[y, x]
 
     def update(self):
+        self.newNumberOfNeighbours = np.copy(self.numberOfNeighbours)
         w, h = self.size
         #newField = [[self._newState((x,y)) for x in range(w)] for y in range(h)]
-        newField = np.zeros((h, w), np.uint8)
+        newField = np.zeros((h, w), np.int8)
         for y in range(h):
             for x in range(w):
                 newField[y, x] = self._newState((x, y))
         self.field = newField
+        self.numberOfNeighbours = self.newNumberOfNeighbours
 
     def loadFromFile(self, path):
         #TODO
+        self._initializeCountNeighbours()
         pass
 
     def print(self):
@@ -64,29 +70,47 @@ class Field():
                 print('#' if self.getPixel(pos)>0 else ' ', end='')
             print('')
 
+    def _setPixel(self, pos, state):
+        x, y = pos
+        self.field[y, x] = state
+
     def _generateField(self, value):
         w, h = self.size
-        field = np.full((h, w), value).astype(np.uint8)
+        field = np.full((h, w), value).astype(np.int8)
         return field
 
     def _newState(self, pos):
-        neighbours = self._countNeighbours(pos)
+        x, y = pos
+
+        neighbours = self.numberOfNeighbours[y, x]
+
+        if neighbours == 0:
+            return 0
+
+        state = self.getPixel(pos)
+        newState = state
 
         if(neighbours < 3):
-            return 0
-            
-        state = self.getPixel(pos)
+            newState = 0 # stirbt an Einsamkeit
 
         if(state == 0 and neighbours == 3):
-            return 1 # wird geboren
-
-        if(state == 1 and neighbours < 3):
-            return 0 # stirbt an Einsamkeit
+            newState = 1 # wird geboren
 
         if(state == 1 and neighbours > 4):
-            return 0 # stirbt wegen Überbevölekrung
+            newState = 0 # stirbt wegen Überbevölekrung
 
-        return state
+        self._updateNeighbourCount(pos, newState-state)
+
+        return newState
+
+    def _updateNeighbourCount(self, pos, diff):
+        if diff == 0:
+            return
+        x, y = pos
+        w, h = self.size
+        x1, x2 = max(x-1, 0), min(x+2, w)
+        y1, y2 = max(y-1, 0), min(y+2, h)
+        self.newNumberOfNeighbours[y1:y2, x1:x2] += diff
 
     def _countNeighbours(self, pos, throughWall = False):
         ''' counts the number of not zero values in the neighbourhood of pos, including pos itself'''
@@ -105,3 +129,10 @@ class Field():
         neighbours = np.count_nonzero(subField)
         return neighbours
 
+    def _initializeCountNeighbours(self):
+        #TODO Optimize
+        w, h = self.size
+        for y in range(h):
+            for x in range(w):
+                pos = x,y
+                self.numberOfNeighbours[y, x] = self._countNeighbours(pos)
